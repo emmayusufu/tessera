86f15756eee6f7d11326f1e307fb4f855f51ab75
Emmanuel Yusufu Kimaswa <kimaswaemma36@gmail.com>
2026-06-02 15:00:00 +0300

    Add tessera token to persist the operator token
    Writes the operator bearer token to ~/.config/tessera/operator-token with 0600 perms so revoke calls don't need it re-exported per shell.

---
+++ cmd/tessera/token.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func cmdToken(args []string) {
+	fs := flag.NewFlagSet("token", flag.ExitOnError)
+	dir := fs.String("config-dir", defaultConfigDir(), "directory to write the operator token into")
+	_ = fs.Parse(args)
+
+	var tok string
+	if fs.NArg() > 0 {
+		tok = fs.Arg(0)
+	} else {
+		fmt.Print("Paste the coordinator operator token: ")
+		line, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil && line == "" {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		tok = line
+	}
+	tok = strings.TrimSpace(tok)
+	if tok == "" {
+		fmt.Fprintln(os.Stderr, "token: refusing to save an empty token")
+		os.Exit(1)
+	}
+
+	check(os.MkdirAll(*dir, 0o700))
+	path := filepath.Join(*dir, "operator-token")
+	check(os.WriteFile(path, []byte(tok), 0o600))
+	fmt.Printf("saved operator token to %s\n", path)
+}
